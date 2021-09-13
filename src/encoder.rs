a0538b4dcadf564fbec2af8fd4adf6685d0c0b3e
saelay <saelay@outlook.jp>
2021-09-13 23:44:35 +0900

    append API

--- src/encoder.rs
+++ src/encoder.rs
@@ -1,5 +1,8 @@
 use crate::document::Document;
 
+use std::ffi::CString;
+
+/// encoder type
 pub enum EncoderType {
     SingleByte,
     DoubleByte,
@@ -7,6 +10,7 @@ pub enum EncoderType {
     Unknown,
 }
 
+/// byte type
 pub enum ByteType {
     Single,
     Lead,
@@ -28,6 +32,7 @@ impl<'a> Encoder<'a> {
         self.enc
     }
 
+    /// Get the type of an encoding object.
     pub fn encoder_type(&self) -> anyhow::Result<EncoderType> {
         let encoder_type = unsafe {
             libharu_sys::HPDF_Encoder_GetType(self.handle())
@@ -41,4 +46,18 @@ impl<'a> Encoder<'a> {
         })
     }
 
+    /// Get the type of byte in the text at position index.
+    pub fn byte_type(&self, text: &str, index: usize) -> anyhow::Result<ByteType> {
+        let text = CString::new(text)?;
+        let byte_type = unsafe {
+            libharu_sys::HPDF_Encoder_GetByteType(self.handle(), text.as_ptr(), index as libharu_sys::HPDF_UINT)
+        };
+
+        Ok(match byte_type {
+            libharu_sys::HPDF_ByteType::HPDF_BYTE_TYPE_SINGLE => ByteType::Single,
+            libharu_sys::HPDF_ByteType::HPDF_BYTE_TYPE_LEAD => ByteType::Lead,
+            libharu_sys::HPDF_ByteType::HPDF_BYTE_TYPE_TRIAL => ByteType::Trial,
+            libharu_sys::HPDF_ByteType::HPDF_BYTE_TYPE_UNKNOWN => ByteType::Unknown,
+        })
+    }
 }
